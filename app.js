e41bab6597f9677bf97f9679c3f06b1a7d89e796
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2018-04-28 21:00:03 -0500

    color change

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var index = 0;                                // index of the currently displaye
 $("#hellos").text(hellos[0]);                 // start by showing a hello
 
 (function animate() {                         // the function responsibe for the animation
-  $("#hellos")
-  // .fadeOut(2000, function() {     // first fadeOut #hellos
-  //   index = (index + 1) % hellos.length;      // when fadeOut complete, increment the index (check if go beyond the length of the array)
-  //   this.textContent = hellos[index];         // change text accordingly
-  // })
-  .fadeIn(2000, animate);                   // then fadeIn. When fadeIn finishes, call animate again
+  $("#hellos").fadeOut(2000, function() {     // first fadeOut #hellos
+    index = (index + 1) % hellos.length;      // when fadeOut complete, increment the index (check if go beyond the length of the array)
+    this.textContent = hellos[index];         // change text accordingly
+  }).fadeIn(2000, animate);                   // then fadeIn. When fadeIn finishes, call animate again
 })();
+
+var options = {
+  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+  typeSpeed: 40
+}
+
+var typed = new Typed(".element", options);
